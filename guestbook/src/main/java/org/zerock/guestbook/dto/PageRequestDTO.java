7b38f0dad3d6dc97ff5ad774659cde3a0258e164
sonhayoni <qheodus123@gmail.com>
2023-03-24 14:19:50 +0900

    4times commit

--- guestbook/src/main/java/org/zerock/guestbook/dto/PageRequestDTO.java
+++ guestbook/src/main/java/org/zerock/guestbook/dto/PageRequestDTO.java
@@ -14,12 +14,16 @@ public class PageRequestDTO {
 
     private int page;
     private int size;
+    private String type;
+    private String keyword;
+
 
     public PageRequestDTO(){
         this.page = 1;
         this.size = 10;
     }
     public Pageable getPageable(Sort sort){
+
         return PageRequest.of(page -1,size,sort);
     }
 }
--- guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -1,5 +1,7 @@
 package org.zerock.guestbook.service;
 
+        import com.querydsl.core.BooleanBuilder;
+        import com.querydsl.core.types.dsl.BooleanExpression;
         import lombok.RequiredArgsConstructor;
         import lombok.extern.log4j.Log4j2;
         import org.springframework.data.domain.Page;
@@ -10,6 +12,7 @@ package org.zerock.guestbook.service;
         import org.zerock.guestbook.dto.PageRequestDTO;
         import org.zerock.guestbook.dto.PageResultDTO;
         import org.zerock.guestbook.entity.Guestbook;
+        import org.zerock.guestbook.entity.QGuestbook;
         import org.zerock.guestbook.repository.GuestbookRepository;
 
         import java.util.Optional;
@@ -39,13 +42,16 @@ public class GuestbookServiceImpl implements GuestbookService{
 
     @Override
     public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO){
+
         Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
 
-        Page<Guestbook> result = repository.findAll(pageable);
+        BooleanBuilder booleanBuilder = getSearch(requestDTO);
+
+        Page<Guestbook> result = repository.findAll(booleanBuilder, pageable);
 
         Function<Guestbook, GuestbookDTO> fn = (entity -> entityToDto(entity));
 
-        return new PageResultDTO<>(result, fn);
+        return new PageResultDTO<>(result,fn);
     }
 
     @Override
@@ -72,4 +78,43 @@ public class GuestbookServiceImpl implements GuestbookService{
             repository.save(entity);
         }
     }
+
+    private BooleanBuilder getSearch(PageRequestDTO requestDTO) { //Querydsl 처리
+
+        String type = requestDTO.getType();
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        QGuestbook qGuestbook = QGuestbook.guestbook;
+
+        String keyword = requestDTO.getKeyword();
+
+        BooleanExpression expression = qGuestbook.gno.gt(0L);
+
+        booleanBuilder.and(expression);
+
+        if(type == null || type.trim().length() == 0){ //검색조건이 없는 경우
+            return booleanBuilder;
+        }
+
+        //검색 조건을 작성하기
+        BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+        if(type.contains("t")){
+            conditionBuilder.or(qGuestbook.title.contains(keyword));
+        }
+        if(type.contains("c")){
+            conditionBuilder.or(qGuestbook.content.contains(keyword));
+        }
+        if(type.contains("w")){
+            conditionBuilder.or(qGuestbook.writer.contains(keyword));
+        }
+
+        //모든 조건 통합
+        booleanBuilder.and(conditionBuilder);
+
+        return booleanBuilder;
+    }
+
+
 }
